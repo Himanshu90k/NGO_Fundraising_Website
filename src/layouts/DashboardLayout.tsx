d8a32cddbc2ed1ec849cf744e4a6775045b05ddf
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-12-25 00:13:04 +0530

    change name

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Dashboard from "../components/dashboard/Dashboard"; 
+import Dashboard from "../components/Dashboard/Dashboard"; 
 
 const DashboardLayout = () => {
     return (
